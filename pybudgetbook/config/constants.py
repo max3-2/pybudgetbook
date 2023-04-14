6f8cc35f4a532ba70e9c4c8d560ddb256371cf35
Max Elfner <maximilian.elfner@kit.edu>
2023-04-14 23:40:06 +0200

    Added mandatory columns to config

--- pybudgetbook/config/constants.py
+++ pybudgetbook/config/constants.py
@@ -1,2 +1,3 @@
 """Containts package wide constants"""
-_MANDATORY_COLS = ['Name', 'Price', 'Group']
+_MANDATORY_COLS = ['ArtNr', 'Name', 'Units', 'PricePerUnit',
+                   'Price', 'TaxClass']
