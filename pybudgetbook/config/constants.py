da86e60359e39a432edec408af86b427c5908089
Max Elfner <maximilian.elfner@kit.edu>
2023-04-15 21:33:33 +0200

    More groups and categories

--- pybudgetbook/config/constants.py
+++ pybudgetbook/config/constants.py
@@ -1,5 +1,5 @@
 """Containts package wide constants"""
-_MANDATORY_COLS = ['ArtNr', 'Name', 'Units', 'PricePerUnit',
-                   'Price', 'TaxClass']
-_CATEGORIES = ['General', 'Food', 'Cars', 'Clothing']
-# Change category general to supermarktet
+_MANDATORY_COLS = ['Date', 'Vendor', 'ArtNr', 'Name', 'Units', 'PricePerUnit',
+                   'Price', 'TaxClass', 'Group', 'Category']
+
+_CATEGORIES = ['Supermarket', 'Cars', 'Clothing', 'Electronics', 'Other']
