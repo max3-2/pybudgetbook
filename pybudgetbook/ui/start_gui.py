9d90e6b313283124a14ae78a3c030f5ccc852c61
Max Elfner <max_elfner@gmx.net>
2023-05-01 21:38:49 +0200

    Added a tray icon

--- pybudgetbook/ui/start_gui.py
+++ pybudgetbook/ui/start_gui.py
@@ -1,17 +1,27 @@
 """Starts the main GUI of the tool from the inherited and customized class"""
 import sys
+import logging
 from pathlib import Path
 from PySide6 import QtWidgets
+from PySide6.QtGui import QIcon
 
 # TODO make package
 sys.path.insert(0, str(Path(__file__).parent.parent.parent))
 from pybudgetbook.ui.main_gui_cust import main_window
 
-
+logger = logging.getLogger(__package__)
 
 def start_main_ui(sys_argv=[]):
     app = QtWidgets.QApplication(sys_argv)
     app.setStyle('Light')
+
+    icon = Path('pybudgetbook/img/tray/tray_icon.svg')
+    if icon.exists():
+        tray_icon = QIcon(str(icon))
+        app.setWindowIcon(tray_icon)
+    else:
+        logger.warning('Tray icon file missing')
+
     qt_main_window = QtWidgets.QMainWindow()
     _ = main_window(qt_main_window)
 
